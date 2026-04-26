d86f7dd7da63b0ab48ac3090080d18f50313ed83
Marvell-Suhali <msuhali@ucsd.edu>
2026-04-25 17:05:28 -0700

    updated global.js

--- global.js
+++ global.js
@@ -71,3 +71,40 @@ if ("colorScheme" in localStorage) {
 select.addEventListener("input", function (event) {
   setColorScheme(event.target.value);
 });
+
+export async function fetchJSON(url) {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch JSON: ${response.statusText}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching or parsing JSON data:", error);
+  }
+}
+
+export function renderProjects(projects, containerElement, headingLevel = "h2") {
+  if (!containerElement) return;
+
+  containerElement.innerHTML = "";
+
+  for (let project of projects) {
+    const article = document.createElement("article");
+
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+      <p>${project.year}</p>
+    `;
+
+    containerElement.append(article);
+  }
+}
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
